d77c8ff0ffbc9acda10f4b57a3ea169cab887a5a
Raghav Sharma <2015eeb1071@iitrpr.ac.in>
2017-05-29 16:17:58 +0530

    Add files via upload

---
+++ linkedListInitialization.cpp
@@ -0,0 +1,47 @@
+// Program to initialize a linked list by adding elements at start
+
+#include<iostream>
+
+using namespace std;
+
+struct Node{
+int data;
+struct Node *next;
+}*newptr,*head,*temp,*np;
+
+struct Node *createNewNode(int x){
+newptr = new Node;
+newptr->data = x;
+newptr->next = NULL;
+return newptr;
+}
+
+void display(struct Node *head){
+	while(head != NULL){
+		cout<<head->data<<"->";
+		head = head->next;
+	}
+cout<<"\n";
+}
+
+void insertNode(int x){
+np = createNewNode(x);
+	if(head == NULL)
+		head = np;
+	else{
+		temp = head;
+		head = np;
+		np->next = temp;
+	}
+}
+
+int main(){
+head = NULL;
+insertNode(1);
+insertNode(2);
+insertNode(3);
+insertNode(4);
+insertNode(5);
+display(head);
+return 0;
+}
