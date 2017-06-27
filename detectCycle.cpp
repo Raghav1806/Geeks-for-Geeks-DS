a45230f4e448123f2104982e784c0ca8d3ee2a60
Raghav Sharma <2015eeb1071@iitrpr.ac.in>
2017-06-27 11:24:02 +0530

    Program to detect a cycle in graph.

---
+++ detectCycle.cpp
@@ -0,0 +1,84 @@
+// program to detect cycle in an undirected graph
+
+/*
+Algorithm:
+
+We do a DFS traversal of the given graph. For every visited vertex ‘v’, if there is an adjacent ‘u’ such that u is already visited and u is not parent of v, then there is a cycle in graph.
+
+Note: The assumption of this approach is that there are no parallel edges between any two vertices.
+*/
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+class Graph{
+	int V;
+	list<int> *adj;
+	bool isCyclicUtil(int v, bool visited[], int parent);
+
+	public:
+		Graph(int V);
+		void addEdge(int v, int w);
+		bool isCyclic();
+};
+
+Graph::Graph(int V){
+	this->V = V;
+	adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w){
+	adj[v].push_back(w);
+	adj[w].push_back(v);
+}
+
+bool Graph::isCyclicUtil(int v, bool visited[], int parent){
+	// mark current node as visited
+	visited[v] = true;
+
+	// recur all vertices adjacent to this vertex
+	list<int>::iterator i;
+	for(i=adj[v].begin(); i!=adj[v].end(); i++){
+		if(!visited[*i]){
+			if(isCyclicUtil(*i, visited, v))
+				return true;
+		}
+
+		// if an adjacent is visited and not parent of current then there is a cycle
+		else if(*i != parent)
+			return true;
+	}
+	return false;
+}
+
+bool Graph::isCyclic(){
+	bool *visited = new bool [V];
+	for(int i=0; i<V; i++)
+		visited[i] = false;
+
+	for(int u=0; u<V; u++)
+		if(!visited[u])
+			if(isCyclicUtil(u, visited, -1))
+				return true;
+	return false;
+}
+
+int main(){
+
+	Graph g1(5);
+	g1.addEdge(1, 0);
+    	g1.addEdge(0, 2);
+    	g1.addEdge(2, 0);
+    	g1.addEdge(0, 3);
+    	g1.addEdge(3, 4);
+    	g1.isCyclic()? cout << "Graph contains cycle\n": cout << "Graph doesn't contain cycle\n";
+ 
+    	Graph g2(3);
+    	g2.addEdge(0, 1);
+    	g2.addEdge(1, 2);
+    	g2.isCyclic()? cout << "Graph contains cycle\n": cout << "Graph doesn't contain cycle\n";
+ 
+return 0;
+}
+
